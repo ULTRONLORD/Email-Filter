35d2f151419a4a09b401234e055a5d9f4dbf2607
Gerald <kaile.gerald@lmu.edu.ng>
2024-03-22 18:49:21 +0100

    Code refactor

--- main.py
+++ main.py
@@ -1,69 +1,22 @@
-lst = ['"',"'",';',"<",">",","," "]
+import re
 path = input("Input the path of the file ")
 file = open(path)
 file_content = file.read()
 
-new = file_content.split(lst[0])
-newstr = ''
-for i in new:
-    newstr += i
-#print(newstr)
-new1 = newstr.split(lst[1])
-new1str = ''
-for i in new1:
-    new1str += i
-#print(new1str)
-new2 = new1str.split(lst[2])
-new2str = ''
-for i in new2:
-    new2str += i
-#print(new2str)
-new3 = new2str.split(lst[3])
-new3str = ''
-for i in new3:
-    new3str += i
-#print(new3str)
-new4 = new3str.split(lst[4])
-new4str = ''
-for i in new4:
-    new4str += i
-#print(4)
-#print(new4str)
+regex = '["<>,\n!#$%&*()^\/]'
+data = re.split(regex, file_content)
+#print(data)
 
-nocomm = new4str.split(lst[5])
-nocommstr = ''
-for i in nocomm:
-    nocommstr = nocommstr + i
+emails = '\w+@'
+res = []
+for names in data:
+    temp = re.search(emails,names)
+    if temp:
+        res.append(names)
 
-
-#print("nocommstr")
-#print(nocommstr)
-
-fin = nocommstr.split("\n")
-#print(fin[1])
-
-final=[]
-for i in fin:
-    if i != '':
-        final.append(i.lower())
-
-#print(final)
-comb = str(final[0])
-#print(comb)
-final.pop(0)
-comblst = comb.split(" ")
-#print(comblst)
-for i in comblst:
-    final.append(i)
-
-print(final)
-
-finish = []
-for i in final:
-    if "@" in i:
-        finish.append(i)
-
-
-f = open("./emails.txt", "w")
-for i in finish:
-    f.write(i + "\n")
+#print(res)
+f = open("./emails.txt",'w')
+for i in res:
+    i = str(i)
+    f.write(i.strip() + "\n")
+f.close()
